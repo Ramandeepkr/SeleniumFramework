ddbe9b488fa121ab53e9c481dba1cc2424ff2b89
raman <ramandeep87.ccit@gmail.com>
2020-02-20 15:48:13 +0530

    hybridframework

--- src/generic/Lib.java
+++ src/generic/Lib.java
@@ -14,81 +14,63 @@ import org.openqa.selenium.WebDriver;
 
 import com.google.common.io.Files;
 
-public class Lib implements IAutoConstant
-{
+public class Lib implements IAutoConstant {
 	public static Workbook wb;
-	public static String getProperty(String key)
-	{
-		String property="";
-		Properties prop=new Properties();
+
+	public static String getProperty(String key) {
+		String property = "";
+		Properties prop = new Properties();
 		try {
 			prop.load(new FileInputStream(CONFIG_PATH));
-			property=prop.getProperty(key);
-			
-			
+			property = prop.getProperty(key);
+
+		} catch (Exception e) {
+
 		}
-		catch(Exception e) {
-			
-			
-		}
-		
+
 		return property;
-		
-		
-		
+
 	}
-	public static int getRowCount(String sheet)
-	{
-		int rowCount=0;
-		try 
-		{
-			wb=WorkbookFactory.create(new FileInputStream(EXCEL_PATH));
-					rowCount=wb.getSheet(sheet).getLastRowNum();
-			
-		}
-		catch(Exception e)
-		{
-					}
-		
-		
-		return rowCount;
-		
-		
-		
-	}
-	public static String getCellValue(String sheet,int row,int column)
-	{
-		
-		String value="";
+
+	public static int getRowCount(String sheet) {
+		int rowCount = 0;
 		try {
-			wb=WorkbookFactory.create(new FileInputStream(EXCEL_PATH));
-			value=wb.getSheet(sheet).getRow(row).getCell(column).toString();
-			
-			
+			wb = WorkbookFactory.create(new FileInputStream(EXCEL_PATH));
+			rowCount = wb.getSheet(sheet).getLastRowNum();
+
+		} catch (Exception e) {
 		}
-		catch(Exception e) {}
-		
-		
-		
-		return value;
-		
-		
-		
+
+		return rowCount;
+
 	}
-	public void takeScreenshot(WebDriver driver,String testname) 
-	{
-		
-		String currentDate=new Date().toString().replaceAll(":","-");
-		TakesScreenshot ts=(TakesScreenshot)driver;
-		File scr=ts.getScreenshotAs(OutputType.FILE);
-		File dest=new File(".\\screenshot\\"+currentDate+"\\"+testname+".png");
+
+	public static String getCellValue(String sheet, int row, int column) {
+
+		String value = "";
+		try {
+			wb = WorkbookFactory.create(new FileInputStream(EXCEL_PATH));
+			value = wb.getSheet(sheet).getRow(row).getCell(column).toString();
+
+		} catch (Exception e) {
+		}
+
+		return value;
+
+	}
+
+	public void takeScreenshot(WebDriver driver, String testname) {
+
+		String currentDate = new Date().toString().replaceAll(":", "-");
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File scr = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File(".\\screenshot\\" + currentDate + "\\" + testname + ".png");
 		try {
 			Files.copy(scr, dest);
 		} catch (IOException e) {
 			e.printStackTrace();
-			
-		}
-	
 
-}
+		}
+
+	}
 }
